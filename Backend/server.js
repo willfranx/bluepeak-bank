c1875143e2c243ceaeac2c0547cca09216671b04
Ahmed <app@air.local>
2025-10-18 18:35:26 -0500

    Setup Docker Compose for frontend, backend, and database with hot-reload

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
