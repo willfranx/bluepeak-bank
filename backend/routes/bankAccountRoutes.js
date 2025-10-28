b7d225474c41741bb33708ed082291e5ec4cbc82
Ahmed <app@air.local>
2025-10-27 23:00:40 -0500

    Add Transaction Controller and Routes (Deposit, Withdraw, Transfer, View History)

---
+++ backend/routes/bankAccountRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { createAccount, getUserAccounts, deleteAccount  } from "../controllers/bankAccountController.js";
+import { protect } from "../middleware/authProtectMiddleware.js"
+
+const router = express.Router();
+
+// Accoount routes
+router.post("/", protect, createAccount);
+router.get("/:userid", protect, getUserAccounts);
+router.delete("/:accountid", protect, deleteAccount);
+
+export default router;
