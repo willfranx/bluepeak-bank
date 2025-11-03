4d5a8b1e51b57bd5b16473f6c8c9b6f290629d28
Ahmed <app@air.local>
2025-11-02 23:54:43 -0800

    Clean up files

--- backend/routes/bankAccountRoutes.js
+++ backend/routes/bankAccountRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { createAccount, getUserAccounts, deleteAccount  } from "../controllers/bankAccountController.js";
 import { protect } from "../middleware/authProtectMiddleware.js"
-import { createAccountInsecure, getUserAccountsInsecure, deleteAccountInsecure } from "../controllers/insecure/insecureBankAccountController.js";
 
 const router = express.Router();
 
@@ -10,9 +9,5 @@ router.post("/", protect, createAccount);
 router.get("/:userid", protect, getUserAccounts);
 router.delete("/:accountid", protect, deleteAccount);
 
-// Account routes (insecure)
-router.post("/insecure/", createAccountInsecure);
-router.get("/insecure/:userid", getUserAccountsInsecure);
-router.delete("/insecure/:accountid", deleteAccountInsecure); 
 
 export default router;
