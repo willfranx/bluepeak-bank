c00113b03cf4b379366f8864733129d681526b40
Ahmed <app@air.local>
2025-11-10 21:15:39 -0800

    Fixed docker connection issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,12 @@ export default defineConfig({
   plugins: [
     tailwindcss(), react()
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost',
+        changeOrigin: true,
+      },
+    },
+  },
 })
