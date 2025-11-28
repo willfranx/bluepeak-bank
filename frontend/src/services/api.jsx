100acc6841c6cd19371bbcfe3d3059818d78a71c
Ahmed Abbas <86217781+Ahmedabbas03@users.noreply.github.com>
2025-11-28 00:27:41 -0800

    Update api.jsx

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = import.meta.env.VITE_API_URL
 
-if (!baseURL) console.warn("VITE_APP_URL is not defined")
+if (!baseURL) console.warn("VITE_API_URL is not defined")
 
 const api = axios.create({
   baseURL: baseURL + '/api',
